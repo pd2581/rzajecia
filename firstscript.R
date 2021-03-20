74218df0f7af96e2abd511ffe0b7c7089a72a7de
pd2581 <pd2581@pjwstk.edu.pl>
2021-03-20 16:38:31 +0100

    v2

---
+++ firstscript.R
@@ -0,0 +1,16 @@
+
+# install.packages("httr")
+# install.packages("jsonlite")
+# 
+# library(httr)
+# require(jsonlite)
+
+endpoint<- "https://api.openweathermap.org/data/2.5/weather?q=Warszawa&appid=1765994b51ed366c506d5dc0d0b07b77"
+getWeather<- GET(endpoint)
+weatherText<- content(getWeather,as ="text")
+weatherJson<- fromJSON(weatherText,flatten = FALSE)
+weatherDF<- as.data.frame(weatherJson)
+View(weatherDF)
+weatherText
+
+#as.data.frame(weatherText)
