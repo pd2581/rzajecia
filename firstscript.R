4d986a1edad30a34b559fe97386839847252340d
pd2581 <pd2581@pjwstk.edu.pl>
2021-03-20 17:34:16 +0100

    v3

--- firstscript.R
+++ firstscript.R
@@ -14,3 +14,43 @@ View(weatherDF)
 weatherText
 
 #as.data.frame(weatherText)
+
+x<-124.5
+x
+class(x)
+is.vector(x)
+x<-"123"
+class(x)
+x<-c(1,2,3,4,5)
+y<-2
+y<-vector(2,mode="integer")
+class(x)
+
+x<-as.integer(x)
+wynik<- x+y
+wynik
+class(wynik)
+
+v1<-c(1,2,3,4,5,6,7,8,9,10)
+v1<-as.integer(v1)
+v2<-as.vector(c(2),mode="integer")
+
+wynik<- v1-v2
+class(wynik)
+wynik<- v1*v2
+class(wynik)
+wynik<- v1+v2
+class(wynik)
+wynik<- v1/v2
+class(wynik)
+
+
+
+wynik<- v1%%v2
+wynik
+wynik<- v1%/%v2
+wynik
+
+lista <- list(1,2,3,4,5)
+class(lista)
+
